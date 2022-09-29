543c1b1bb4de078407bfa7d4df9ba2ef0bb4e43b
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2022-09-29 10:39:06 -0700

    Update streamlit_app.py
    typo_fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menue")
 streamlit.text("Oatmeal with fresh fruit and maple syrup")
 streamlit.text("Kale and Spinach Rocket Smoothie")
-streamlit.text("Free Range Eggs with wheat toast and Fresh Fruit')
+streamlit.text("Free Range Eggs with wheat toast and Fresh Fruit")
       
