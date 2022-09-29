8961de0c8a86b62e3484cab61fe0e83193d8e02d
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2022-09-29 10:43:47 -0700

    Update streamlit_app.py
    emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ streamlit.title("My Parents New Healthy Diner")
 
 
 streamlit.header("Breakfast Menue")
-streamlit.text("Oatmeal with fresh fruit and maple syrup")
-streamlit.text("Kale and Spinach Rocket Smoothie")
-streamlit.text("Free Range Eggs with wheat toast and Fresh Fruit")
-      
+streamlit.text("🥣 Oatmeal with fresh fruit and maple syrup")
+streamlit.text("🥗 Kale and Spinach Rocket Smoothie")
+streamlit.text("🐔 Free Range Eggs with wheat toast and Fresh Fruit")
+streamlit.text("🥑🍞 Avacado Toast")
+
+
