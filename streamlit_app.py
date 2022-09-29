6fa793fec820d0166c3b380ff198e519761bd958
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2022-09-29 10:36:45 -0700

    Update streamlit_app.py
    healthy options updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+
+
+streamlit.header("Breakfast Menue")
+streamlit.text("Oatmeal with fresh fruit and maple syrup")
+streamlit.text("Kale and Spinach Rocket Smoothie")
+streamlit.text("Free Range Eggs with wheat toast and Fresh Fruit')
+      
