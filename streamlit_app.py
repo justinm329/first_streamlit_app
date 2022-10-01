c0dc852d2ff9fd50c24dfe1e3c693f23c0e13cd7
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2022-10-01 16:01:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 #streamlit.write("The user entered", fruit_choice)
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
