c5f53874c8692a102d260360c0722920cd329863
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2022-10-01 16:23:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_reponse = requests.get('https://fruityvice.com/api/fruit/'+ this_fruit_choice)
-  #use pandas to normalize the json format
+#use pandas to normalize the json format
   fruityvice_normalized = pd.json_normalize(fruityvice_reponse.json())
   return fruityvice_nomalized
 #NEW section to display fruityvice api response
@@ -32,21 +32,21 @@ streamlit.header("Fruity Vice Fruit Advice!!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like infomration about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
+        streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
 #streamlit.header("The fruit load list contains:")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
 # add a button to load the fruit
 if stremlit.button("Get Fruit Load List"):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
 ##my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
